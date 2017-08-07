b55b91d2ed9ac0661d758d596ded9fd9f93e38a8
andrey-katentsev <hyperlink@inbox.ru>
2017-08-07 18:48:59 +0300

    MainClass and ArrayListTest deleted; CustomerTest added.

---
+++ src/test/java/com/scrumtrek/simplestore/demo/CustomerTest.java
@@ -0,0 +1,49 @@
+package com.scrumtrek.simplestore.demo;
+
+import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.Rental;
+import org.junit.Test;
+
+import static com.scrumtrek.simplestore.PriceCodes.Regular;
+import static junit.framework.TestCase.assertEquals;
+
+public class CustomerTest {
+    @Test
+    public void shouldHaveNameWhenCreated(){
+        //region Fixture | Arrange | Given
+        //endregion
+
+        //region Act | When
+        Customer uut = new Customer("John");
+        //endregion
+
+        //region Assert | Then
+        assertEquals("John", uut.getName());
+        //endregion
+    }
+
+    @Test
+    public void shouldGenerateStatementWhenSingleMovieOneDayRental(){
+        //region Fixture | Arrange | Given
+        Customer uut = new Customer("John");
+        Movie kingKong = new Movie("King Kong", Regular);
+        Rental kingKongRental = new Rental( kingKong, 1);
+
+        uut.addRental(kingKongRental);
+        //endregion
+
+        //region Act | When
+        String statement = uut.Statement();
+        //endregion
+
+        String expected = "Rental record for John\n" +
+                "\tKing Kong\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points.";
+
+        //region Assert | Then
+        assertEquals(expected, statement);
+        //endregion
+    }
+}
