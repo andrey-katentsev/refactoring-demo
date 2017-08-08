1eff953b03cc86a759445ac40ecc07e894c2be36
andrey-katentsev <hyperlink@inbox.ru>
2017-08-08 13:18:31 +0300

    CustomerTest TwoDaysRentalTest with Movie mock added.

--- src/test/java/com/scrumtrek/simplestore/demo/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/demo/CustomerTest.java
@@ -3,10 +3,14 @@ package com.scrumtrek.simplestore.demo;
 import com.scrumtrek.simplestore.Customer;
 import com.scrumtrek.simplestore.Movie;
 import com.scrumtrek.simplestore.Rental;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static com.scrumtrek.simplestore.PriceCodes.NewRelease;
 import static com.scrumtrek.simplestore.PriceCodes.Regular;
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CustomerTest {
     @Test
@@ -41,6 +45,34 @@ public class CustomerTest {
                 "\tKing Kong\t2.0\n" +
                 "Amount owed is 2.0\n" +
                 "You earned 1 frequent renter points.";
+
+        //region Assert | Then
+        assertEquals(expected, statement);
+        //endregion
+    }
+
+    @Test
+    public void shouldGenerateStatementWhenSingleMovieTwoDayRental(){
+        //region Fixture | Arrange | Given
+        Customer uut = new Customer("John");
+        //Movie kingKong = new Movie("King Kong", Regular);
+        Movie stub = mock(Movie.class);
+        when(stub.getPriceCode()).thenReturn(NewRelease);
+        when(stub.getTitle()).thenReturn("Mock Movie");
+
+        Rental stubRental = new Rental( stub, 2);
+
+        uut.addRental(stubRental);
+        //endregion
+
+        //region Act | When
+        String statement = uut.Statement();
+        //endregion
+
+        String expected = "Rental record for John\n" +
+                "\tMock Movie\t6.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter points.";
 
         //region Assert | Then
         assertEquals(expected, statement);
